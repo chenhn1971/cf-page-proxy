b9b791fd50f370faaa428ca6b61b2cfd7a3fb0f2
chenhn1971 <143401333+chenhn1971@users.noreply.github.com>
2024-03-02 08:36:10 +0800

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -2,7 +2,7 @@ export default {
     async fetch(request, env) {
       let url = new URL(request.url);
       if (url.pathname.startsWith('/')) {
-        url.hostname="wiiu.1919810.com";
+        url.hostname="chenhn1971.github.io";
         let new_request=new Request(url,request);
         return fetch(new_request);
       }
